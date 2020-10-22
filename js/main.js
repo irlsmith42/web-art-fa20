a5b319630d134d24716d15275c9aec5ff52ff885
IRLSmith42 <70673988+irlsmith42@users.noreply.github.com>
2020-10-21 21:22:19 -0500

    Oh what

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $(function(){
+    var note = $('#note'),
+    ts = new Date(2999, 12, 31);
+    
+    $('#countdown').countdown({
+    timestamp : ts,
+    callback : function(days, hours, minutes, seconds){
+    
+    var message = "";
+    message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+    message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+    message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+    message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+    message += "left for 1st August!";
+    note.html(message);
+    }
+    });
+    
+    });
+
+})
